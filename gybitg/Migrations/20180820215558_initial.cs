7f45318e08811d2593579398237771648c47e4f2
Brian Bansenauer <brianb@cascadia.edu>
2018-10-28 13:06:46 -0700

    reverting

--- gybitg/Migrations/20180820215558_initial.cs
+++ gybitg/Migrations/20180820215558_initial.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
 
 namespace gybitg.Migrations
 {
@@ -12,10 +13,10 @@ namespace gybitg.Migrations
                 name: "AspNetRoles",
                 columns: table => new
                 {
-                    Id = table.Column<string>(nullable: false),
-                    Name = table.Column<string>(maxLength: 256, nullable: true),
-                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
-                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
                 },
                 constraints: table =>
                 {
@@ -26,28 +27,28 @@ namespace gybitg.Migrations
                 name: "AspNetUsers",
                 columns: table => new
                 {
-                    Id = table.Column<string>(nullable: false),
-                    UserName = table.Column<string>(maxLength: 256, nullable: true),
-                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
-                    Email = table.Column<string>(maxLength: 256, nullable: true),
-                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
-                    EmailConfirmed = table.Column<bool>(nullable: false),
-                    PasswordHash = table.Column<string>(nullable: true),
-                    SecurityStamp = table.Column<string>(nullable: true),
-                    ConcurrencyStamp = table.Column<string>(nullable: true),
-                    PhoneNumber = table.Column<string>(nullable: true),
-                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
-                    TwoFactorEnabled = table.Column<bool>(nullable: false),
-                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
-                    LockoutEnabled = table.Column<bool>(nullable: false),
-                    AccessFailedCount = table.Column<int>(nullable: false),
-                    FirstName = table.Column<string>(nullable: true),
-                    LastName = table.Column<string>(nullable: true),
-                    Position = table.Column<string>(nullable: true),
-                    City = table.Column<string>(nullable: true),
-                    State = table.Column<string>(nullable: true),
-                    Zip = table.Column<string>(nullable: true),
-                    AvatarImageUrl = table.Column<string>(nullable: true)
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false),
+                    AvatarImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Zip = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -58,15 +59,15 @@ namespace gybitg.Migrations
                 name: "AthleteProfiles",
                 columns: table => new
                 {
-                    UserId = table.Column<string>(nullable: false),
-                    DateOfBirth = table.Column<DateTime>(nullable: false),
-                    Height = table.Column<decimal>(nullable: false),
-                    Weight = table.Column<decimal>(nullable: false),
-                    PersnalBio = table.Column<string>(nullable: true),
-                    HighschoolName = table.Column<string>(nullable: true),
-                    HighschoolCoach = table.Column<string>(nullable: true),
-                    HSGraduationDate = table.Column<DateTime>(nullable: false),
-                    AAUId = table.Column<string>(nullable: true)
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AAUId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HSGraduationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Height = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    HighschoolCoach = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HighschoolName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersnalBio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Weight = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -77,25 +78,25 @@ namespace gybitg.Migrations
                 name: "AthleteStats",
                 columns: table => new
                 {
-                    Id = table.Column<int>(nullable: false)
+                    Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    UserId = table.Column<string>(nullable: false),
-                    DateOFEntry = table.Column<DateTime>(nullable: false),
-                    PPG = table.Column<decimal>(nullable: false),
-                    RPG = table.Column<decimal>(nullable: false),
-                    APG = table.Column<decimal>(nullable: false),
-                    GP = table.Column<int>(nullable: false),
-                    GS = table.Column<int>(nullable: false),
-                    MPG = table.Column<decimal>(nullable: false),
-                    FGAG = table.Column<decimal>(nullable: false),
-                    FGMG = table.Column<decimal>(nullable: false),
-                    FGG = table.Column<decimal>(nullable: false),
-                    FTAG = table.Column<decimal>(nullable: false),
-                    FTMG = table.Column<decimal>(nullable: false),
-                    FTP = table.Column<decimal>(nullable: false),
-                    TPAG = table.Column<decimal>(nullable: false),
-                    TPMG = table.Column<decimal>(nullable: false),
-                    TPP = table.Column<decimal>(nullable: false)
+                    APG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    DateOFEntry = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FGAG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    FGG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    FGMG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    FTAG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    FTMG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    FTP = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    GP = table.Column<int>(type: "int", nullable: false),
+                    GS = table.Column<int>(type: "int", nullable: false),
+                    MPG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    PPG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    RPG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    TPAG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    TPMG = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    TPP = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -106,16 +107,16 @@ namespace gybitg.Migrations
                 name: "CoachProfiles",
                 columns: table => new
                 {
-                    Id = table.Column<int>(nullable: false)
+                    Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    UserId = table.Column<string>(nullable: false),
-                    AAUId = table.Column<string>(nullable: true),
-                    PersnalBio = table.Column<string>(nullable: true),
-                    Wins = table.Column<int>(nullable: false),
-                    Lossess = table.Column<int>(nullable: false),
-                    YearsCoaching = table.Column<decimal>(nullable: false),
-                    Achievments = table.Column<string>(nullable: true),
-                    Verified = table.Column<bool>(nullable: false)
+                    AAUId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Achievments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lossess = table.Column<int>(type: "int", nullable: false),
+                    PersnalBio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Verified = table.Column<bool>(type: "bit", nullable: false),
+                    Wins = table.Column<int>(type: "int", nullable: false),
+                    YearsCoaching = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -126,11 +127,11 @@ namespace gybitg.Migrations
                 name: "AspNetRoleClaims",
                 columns: table => new
                 {
-                    Id = table.Column<int>(nullable: false)
+                    Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    RoleId = table.Column<string>(nullable: false),
-                    ClaimType = table.Column<string>(nullable: true),
-                    ClaimValue = table.Column<string>(nullable: true)
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -147,11 +148,11 @@ namespace gybitg.Migrations
                 name: "AspNetUserClaims",
                 columns: table => new
                 {
-                    Id = table.Column<int>(nullable: false)
+                    Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    UserId = table.Column<string>(nullable: false),
-                    ClaimType = table.Column<string>(nullable: true),
-                    ClaimValue = table.Column<string>(nullable: true)
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -168,10 +169,10 @@ namespace gybitg.Migrations
                 name: "AspNetUserLogins",
                 columns: table => new
                 {
-                    LoginProvider = table.Column<string>(nullable: false),
-                    ProviderKey = table.Column<string>(nullable: false),
-                    ProviderDisplayName = table.Column<string>(nullable: true),
-                    UserId = table.Column<string>(nullable: false)
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -188,8 +189,8 @@ namespace gybitg.Migrations
                 name: "AspNetUserRoles",
                 columns: table => new
                 {
-                    UserId = table.Column<string>(nullable: false),
-                    RoleId = table.Column<string>(nullable: false)
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -212,10 +213,10 @@ namespace gybitg.Migrations
                 name: "AspNetUserTokens",
                 columns: table => new
                 {
-                    UserId = table.Column<string>(nullable: false),
-                    LoginProvider = table.Column<string>(nullable: false),
-                    Name = table.Column<string>(nullable: false),
-                    Value = table.Column<string>(nullable: true)
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
