7a3397743c01438691b85da3c2d25dcf9507545c
Brenda Bruil <bjbrui@aol.com>
2018-12-01 14:16:07 -0800

    changed startup.cs default route.  Also had to change launchsettings.json that changee won't get pushed.

--- gybitg/Startup.cs
+++ gybitg/Startup.cs
@@ -106,7 +106,7 @@ namespace gybitg
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
