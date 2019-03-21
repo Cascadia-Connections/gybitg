66651db14ec69cd5da31628a01c82b480c8f3024
Brenda Bruil <bjbrui@aol.com>
2019-03-21 16:59:24 -0700

    Added Coach Controller and Views, Linking table for following.

---
+++ gybitg/Controllers/CoachController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gybitg.Models;
+using gybitg.Models.ManageViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using gybitg.Data;
+
+namespace gybitg.Controllers
+{
+    public class CoachController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+
+        public CoachController(
+          UserManager<ApplicationUser> userManager,
+          SignInManager<ApplicationUser> signInManager,
+          RoleManager<IdentityRole> roleManager,
+          ApplicationDbContext context
+         )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AthleteList()
+        {
+            string roleName = "Athlete";
+
+            var usersOfRole = await _userManager.GetUsersInRoleAsync(roleName);
+
+            List<AthleteUserViewModel> athletes = new List<AthleteUserViewModel>();
+
+            foreach (var u in usersOfRole)
+            {
+                AthleteUserViewModel au = new AthleteUserViewModel();
+                au.UserId = u.Id;
+                au.FirstName = u.FirstName;
+                au.LastName = u.LastName;
+                au.Position = u.Position;
+                au.AvatarImageUrl = u.AvatarImageUrl;
+                au.ProfileVideoUrl = u.ProfileVideoUrl;
+
+                athletes.Add(au);
+
+
+            }
+
+            return View(athletes);
+
+        }
+
+        [HttpGet]
+        public IActionResult Follow()
+        {
+            //string roleName = "Athlete";
+
+            //var usersOfRole = await _userManager.GetUsersInRoleAsync(roleName);
+
+            //List<AthleteUserViewModel> athletes = new List<AthleteUserViewModel>();
+
+            //foreach (var u in usersOfRole)
+            //{
+            //    AthleteUserViewModel au = new AthleteUserViewModel();
+            //    au.UserId = u.UserName;
+            //    au.FirstName = u.FirstName;
+            //    au.LastName = u.LastName;
+            //    au.Position = u.Position;
+            //    au.AvatarImageUrl = u.AvatarImageUrl;
+            //    au.ProfileVideoUrl = u.ProfileVideoUrl;
+
+            //    athletes.Add(au);
+
+
+            //}
+
+            return View();
+
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> AthleteList(string SearchString)
+        //{
+        //    string roleName = "Athlete";
+
+        //    var usersOfRole = await _userManager.GetUsersInRoleAsync(roleName);
+
+        //         List<AthleteUserViewModel> searchusers = new List<AthleteUserViewModel>();
+
+        //    if (!string.IsNullOrEmpty(SearchString))
+        //    {
+        //        foreach (var u in usersOfRole)
+        //    {
+        //            if (u.LastName.Contains(SearchString) || u.FirstName.Contains(SearchString)|| u.Position.Contains(SearchString))
+        //            {
+        //                AthleteUserViewModel sau = new AthleteUserViewModel();
+        //                sau.FirstName = u.FirstName;
+        //                sau.LastName = u.LastName;
+        //                sau.Position = u.Position;
+        //                sau.AvatarImageUrl = u.AvatarImageUrl;
+        //                sau.ProfileVideoUrl = u.ProfileVideoUrl;
+        //                searchusers.Add(sau);
+        //            }
+
+        //    } if (string.IsNullOrEmpty(SearchString))
+        //            foreach (var u in usersOfRole)
+        //            {
+
+        //                    AthleteUserViewModel sau = new AthleteUserViewModel();
+        //                    sau.FirstName = u.FirstName;
+        //                    sau.LastName = u.LastName;
+        //                    sau.Position = u.Position;
+        //                    sau.AvatarImageUrl = u.AvatarImageUrl;
+        //                    sau.ProfileVideoUrl = u.ProfileVideoUrl;
+        //                    searchusers.Add(sau);
+
+
+        //            }
+
+        //    }
+        //    return View(searchusers);
+        //}
+
+
+
+        [HttpPost]
+        public IActionResult Follow(string UserId)
+        {
+         
+
+            CoachAthlete following = new CoachAthlete();
+            following.AthleteId = UserId;
+            following.Athlete = _context.AthleteProfiles.SingleOrDefault(a => a.UserId == UserId);
+           
+            following.CoachId = _userManager.GetUserId(User);
+            following.Coach = _context.CoachProfiles.SingleOrDefault(c => c.UserId == following.CoachId);
+
+            _context.CoachAthletes.Add(following);
+            _context.SaveChanges();
+
+            return View();
+
+        }
+    }
+
+
+}
---
+++ gybitg/Models/ManageViewModels/AthleteUserViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace gybitg.Models.ManageViewModels
+{
+    public class AthleteUserViewModel
+    {
+        [Key]
+        public string UserId { get; set; }
+
+        [Display(Name = "First Name")]
+        public virtual string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public virtual string LastName { get; set; }
+
+        public string Text { get; set; }
+
+        public virtual string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public string Position { get; set; }
+
+        public string AvatarImageUrl { get; set; }
+
+        public string ProfileVideoUrl { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
+        [Display(Name = "HS Graduation Date"), DataType(DataType.Date)]
+        public DateTime HSGraduationDate { get; set; }
+    }
+}
