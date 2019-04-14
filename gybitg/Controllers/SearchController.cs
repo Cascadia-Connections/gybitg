db7ba07b430cdf2228df87f10ce984948cfbca57
Watkins, Daniel R <danielwatkins@student.cascadia.edu>
2019-04-13 18:22:28 -0700

    Added controller called SearchController and a viewmodel called SearchResultsViewModel to Models\SearchViewmodels folder that I made. The viewmodel should be good to go and I am currently working on the controller.

---
+++ gybitg/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using gybitg.Models.SearchViewModels;
+using gybitg.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gybitg.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IAthleteProfileRepository athleteRepository;
+        private IApplicationUserRepository userRepository;
+        private IAthleteStatsRepository statsRepository;
+
+        public ViewResult SearchResults()         
+            => View(new SearchResultsViewModel
+            {
+
+            });
+        
+    }
+}
---
+++ gybitg/Models/SearchViewModels/SearchResultsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gybitg.Models;
+
+namespace gybitg.Models.SearchViewModels
+{
+    public class SearchResultsViewModel
+    {
+        public IEnumerable<ApplicationUser> Users { get; set; }
+        public IEnumerable<AthleteProfile> AthletesProfile { get; set; }
+        public IEnumerable<AthleteStats> AthleteStats { get; set; }
+    }
+}
