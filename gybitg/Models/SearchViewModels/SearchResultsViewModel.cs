ed2425f35c24c76e88526cbb7bb9f7453911da38
Alex Sparks <alex.sparks90@yahoo.com>
2019-04-15 13:22:48 -0700

    Fixed naming conventions and added a dropdown for positions

--- gybitg/Models/SearchViewModels/SearchResultsViewModel.cs
+++ gybitg/Models/SearchViewModels/SearchResultsViewModel.cs
@@ -21,7 +21,7 @@ namespace gybitg.Models.SearchViewModels
         public virtual string FullName { get; set; }
 
         [Display(Name = "Position")]
-        public string Position { get; set; }
+        public PositionType Position { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
         [Display(Name = "HS Graduation Date"), DataType(DataType.Date)]
@@ -38,5 +38,14 @@ namespace gybitg.Models.SearchViewModels
 
         [Display(Name = "Free Throws Made per Game")]
         public decimal FTMG { get; set; }
+
+        public enum PositionType
+        {
+            PointGuard,
+            ShootingGuard,
+            SmallForward,
+            PowerForward,
+            Center
+        }
     }
 }
