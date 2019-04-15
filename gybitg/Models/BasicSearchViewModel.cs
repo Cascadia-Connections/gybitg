70f9a4bc8190ecbb8239bdc43953ce86268ae338
ksdurg <ksdurg@hotmail.com>
2019-04-15 10:21:11 -0700

    Added a BasicSearchViewModel with 3 basic variables (first name, last name, postion) and a BasicSearch view under the Coaches views folder.

---
+++ gybitg/Models/BasicSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace gybitg.ViewModels
+{
+    public class BasicSearchViewModel
+    {
+        [Display(Name = "First Name")]
+        public virtual string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public virtual string LastName { get; set; }
+
+        [Display(Name = "Position")]
+        public string Position { get; set; }
+    }
+}
