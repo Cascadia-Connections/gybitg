455b8f89eb02a7f6f4754b17c05fb79492435b06
Alex Sparks <alex.sparks90@yahoo.com>
2019-04-17 14:27:23 -0700

    Fixed searchresultsview

--- gybitg/Models/SearchViewModels/SearchResultsViewModel.cs
+++ gybitg/Models/SearchViewModels/SearchResultsViewModel.cs
@@ -22,7 +22,7 @@ namespace gybitg.Models.SearchViewModels
         public virtual string FullName { get; set; }
 
         [Display(Name = "Position")]
-        public PositionType Position { get; set; }
+        public String Position { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
         [Display(Name = "HS Graduation Date"), DataType(DataType.Date)]
@@ -40,13 +40,6 @@ namespace gybitg.Models.SearchViewModels
         [Display(Name = "Free Throws Made per Game")]
         public decimal FTMG { get; set; }
 
-        public enum PositionType
-        {
-            PointGuard,
-            ShootingGuard,
-            SmallForward,
-            PowerForward,
-            Center
-        }
+
     }
 }
