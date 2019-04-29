d282c5a6b9e79eb605fb97c9ea6a6aab9c4dfee3
Adam Kohler <adamkohler@frontier.com>
2019-04-29 11:47:46 -0700

    added couple more athleteProfiles

--- gybitg/Models/Repositories/FakeAthleteRepository.cs
+++ gybitg/Models/Repositories/FakeAthleteRepository.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using gybitg.Models;
 
 namespace gybitg.Models.Repositories
 {
     public class FakeAthleteRepository : IAthleteRepository
     {
-        //Is this workable?  Can I have these two things be populated by the seed data
-        // I have in another file?
         public IQueryable<AthleteProfile> athleteProfiles => new List<AthleteProfile>
         {
-            new AthleteProfile {HighschoolName = "Bothell", HSGraduationDate = new DateTime(2020-06), UserId = "1", HighschoolCoach = "Bob" }, //, ClubCoach = "Mary", Postition = "PointGuard"},
-            new AthleteProfile {HighschoolName = "Inglemoor", HSGraduationDate = new DateTime(2021-06), UserId = "2", HighschoolCoach = "Cindy" }, //, ClubCoach = "Susan", Position = " ShootingGuard"},
-            new AthleteProfile {HighschoolName = "Woodinville", HSGraduationDate = new DateTime(2021-06), UserId = "3", HighschoolCoach = "Prudence" }, //, ClubCoach = "Sarah", Position ="SmallForward"},
-            new AthleteProfile {HighschoolName = "Jackson", HSGraduationDate = new DateTime(2023-06), UserId = "4", HighschoolCoach = "Zack" }, // ClubCoach = "Emily", Postition = "PowerForward"},
-            new AthleteProfile {HighschoolName = "Juanita", HSGraduationDate = new DateTime(2023-06), UserId = "5", HighschoolCoach = "Teresa" }, // ClubCoach = "Tina", Postion = "Center"},      
+            new AthleteProfile {HighschoolName = "Bothell", HSGraduationDate = new DateTime(2020-06), UserId = "1", HighschoolCoach = "Bob" }, //, ClubCoach = "Mary", Postition = "Point Guard"},
+            new AthleteProfile {HighschoolName = "Inglemoor", HSGraduationDate = new DateTime(2021-06), UserId = "2", HighschoolCoach = "Cindy" }, //, ClubCoach = "Susan", Position = "Shooting Guard"},
+            new AthleteProfile {HighschoolName = "Woodinville", HSGraduationDate = new DateTime(2021-06), UserId = "3", HighschoolCoach = "Prudence" }, //, ClubCoach = "Sarah", Position ="Small Forward"},
+            new AthleteProfile {HighschoolName = "Jackson", HSGraduationDate = new DateTime(2023-06), UserId = "4", HighschoolCoach = "Zack" }, // ClubCoach = "Emily", Position = "Power Forward"},
+            new AthleteProfile {HighschoolName = "Juanita", HSGraduationDate = new DateTime(2023-06), UserId = "5", HighschoolCoach = "Teresa" }, // ClubCoach = "Tina", Position = "Center"},
+            new AthleteProfile {HighschoolName = "Bothell", HSGraduationDate = new DateTime(2023-96), UserId = "6", HighschoolCoach = "Sally"}, // ClubCoach = "Ashley", Position = "Power Forward"},
+            new AthleteProfile {HighschoolName = "Mariner", HSGraduationDate = new DateTime(2021-06), UserId = "7", HighschoolCoach = "Betty"} //, ClubCoach = "Denise", Position = "Center"}
 
 
         }.AsQueryable<AthleteProfile>();
