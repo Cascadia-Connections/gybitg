44e160792e79954cbc25e871e349188be481f88e
Adam Kohler <adamkohler@frontier.com>
2019-04-29 10:46:32 -0700

    updated profiles with club coach, added applicationUser objects with a position for the athletes

--- gybitg/Models/Repositories/FakeAthleteRepository.cs
+++ gybitg/Models/Repositories/FakeAthleteRepository.cs
@@ -12,14 +12,24 @@ namespace gybitg.Models.Repositories
         // I have in another file?
         public IQueryable<AthleteProfile> athleteProfiles => new List<AthleteProfile>
         {
-            new AthleteProfile {HighschoolName = "Bothell", HSGraduationDate = new DateTime(2020-06), UserId = "1", HighschoolCoach = "Bob" }, //, ClubCoach = "Mary"},
-            new AthleteProfile {HighschoolName = "Inglemoor", HSGraduationDate = new DateTime(2021-06), UserId = "2", HighschoolCoach = "Cindy" }, //, ClubCoach = "Susan"},
-            new AthleteProfile {HighschoolName = "Woodinville", HSGraduationDate = new DateTime(2021-06), UserId = "3", HighschoolCoach = "Prudence" }, //, ClubCoach = "Sarah" },
-            new AthleteProfile {HighschoolName = "Jackson", HSGraduationDate = new DateTime(2023-06), UserId = "4", HighschoolCoach = "Zack" }, // ClubCoach = "Emily" },
-            new AthleteProfile {HighschoolName = "Juanita", HSGraduationDate = new DateTime(2023-06), UserId = "5", HighschoolCoach = "Teresa" }, // ClubCoach = "Tina" },
+            new AthleteProfile {HighschoolName = "Bothell", HSGraduationDate = new DateTime(2020-06), UserId = "1", HighschoolCoach = "Bob" }, //, ClubCoach = "Mary", Postition = "PointGuard"},
+            new AthleteProfile {HighschoolName = "Inglemoor", HSGraduationDate = new DateTime(2021-06), UserId = "2", HighschoolCoach = "Cindy" }, //, ClubCoach = "Susan", Position = " ShootingGuard"},
+            new AthleteProfile {HighschoolName = "Woodinville", HSGraduationDate = new DateTime(2021-06), UserId = "3", HighschoolCoach = "Prudence" }, //, ClubCoach = "Sarah", Position ="SmallForward"},
+            new AthleteProfile {HighschoolName = "Jackson", HSGraduationDate = new DateTime(2023-06), UserId = "4", HighschoolCoach = "Zack" }, // ClubCoach = "Emily", Postition = "PowerForward"},
+            new AthleteProfile {HighschoolName = "Juanita", HSGraduationDate = new DateTime(2023-06), UserId = "5", HighschoolCoach = "Teresa" }, // ClubCoach = "Tina", Postion = "Center"},      
+
 
         }.AsQueryable<AthleteProfile>();
 
+        public IQueryable<ApplicationUser> applicationUser => new List<ApplicationUser>
+        {
+            //Will this link with the above athleteProfiles?
+            new ApplicationUser {Position = "Point Guard"},
+            new ApplicationUser {Position = "Shooting Guard"},
+            new ApplicationUser {Position = "Small Forward"},
+            new ApplicationUser {Position = "Power Forward"},
+            new ApplicationUser {Position = "Center Forward"}
+        }.AsQueryable<ApplicationUser>();
 
 
         public IQueryable<AthleteStats> athleteStats => new List<AthleteStats>
