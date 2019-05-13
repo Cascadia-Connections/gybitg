ae29c82a62f6b4955c65bd677478452c5fff4615
Daniel-Watkins <danielroywatkins@gmail.com>
2019-05-13 11:12:54 -0700

    Made test project and search test file

---
+++ gybitg.Tests/SearchTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using gybitg.Controllers;
+using gybitg.Models;
+
+namespace gybitg.Tests
+{
+    public class SearchTests
+    {
+        [Fact]
+        public void SearchResultsReturnsRightAthlete()
+        {
+
+        }
+    }
+}
