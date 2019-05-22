e76d10ffc9fcde59b6592bce01a152ce7174753b
Alex Sparks <alex.sparks90@yahoo.com>
2019-05-22 12:46:58 -0700

    Changing dob and hsgraddate to strings

--- gybitg/Models/AthleteProfile.cs
+++ gybitg/Models/AthleteProfile.cs
@@ -12,9 +12,9 @@ namespace gybitg.Models
         [Key]
         public string UserId { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Date of Birth"), DataType(DataType.Date)]
-        public DateTime DateOfBirth { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}"/*, ApplyFormatInEditMode = true*/)]
+        [Display(Name = "Date of Birth")/*, DataType(DataType.Date)*/]
+        public string DateOfBirth { get; set; }
 
         [Display(Name = "Height (In.)")]
         public decimal Height { get; set; }
@@ -31,9 +31,9 @@ namespace gybitg.Models
         [Display(Name = "Current Highschool Coach")]
         public string HighschoolCoach { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
-        [Display(Name = "HS Graduation Date"), DataType(DataType.Date)]
-        public DateTime HSGraduationDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM}"/*, ApplyFormatInEditMode = true*/)]
+        [Display(Name = "HS Graduation Date")/*, DataType(DataType.Date)*/]
+        public string HSGraduationDate { get; set; }
 
         [Display(Name = "AAU Team")]
         public string AAUId { get; set; }
