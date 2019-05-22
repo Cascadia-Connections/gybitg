38951e2bbacce0ff5cc9506c51936df3fb870629
keithrutler <keithrutler@gmail.com>
2019-05-22 11:48:07 -0700

    Added [Required] to athlete profile view model

--- gybitg/Models/ManageViewModels/AthleteProfileViewModel.cs
+++ gybitg/Models/ManageViewModels/AthleteProfileViewModel.cs
@@ -11,11 +11,13 @@ namespace gybitg.Models.ManageViewModels
         [Key]
         public string UserId { get; set; }
 
+        [Required]
         [RegularExpression("^[a-zA-Z .&'-]*$", ErrorMessage = "Only Alphabetical characters allowed.")]
         [StringLength(20, ErrorMessage = "First Name should be less than or equal to twenty characters.")]
         [Display(Name = "First Name")]
         public virtual string FirstName { get; set; }
 
+        [Required]
         [RegularExpression("^[a-zA-Z .&'-]*$", ErrorMessage = "Only Alphabetical characters allowed.")]
         [StringLength(20, ErrorMessage = "Last Name should be less than or equal to twenty characters.")]
         [Display(Name = "Last Name")]
@@ -25,11 +27,11 @@ namespace gybitg.Models.ManageViewModels
         [Display(Name = "Date of Birth"), DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
-        [Range(1, 100, ErrorMessage = "Height can be between 1 to 100 inches." )]
+        [Range(1, 999, ErrorMessage = "Height can be between 1 to 999 inches." )]
         [Display(Name = "Height (inches)")]
         public decimal Height { get; set; }
 
-        [Range(1, 200, ErrorMessage = "Weight can be between 1 to 200 Lbs.")]
+        [Range(1, 999, ErrorMessage = "Weight can be between 1 to 999 Lbs.")]
         [Display(Name = "Weight (Lbs.)")]
         public decimal Weight { get; set; }
 
@@ -38,16 +40,19 @@ namespace gybitg.Models.ManageViewModels
         [Display(Name = "Personal Bio")]
         public string PersonalBio { get; set; }
 
+        [Required]
         [RegularExpression("^[a-zA-Z .&'-]*$", ErrorMessage = "Only Alphabetical characters allowed.")]
         [StringLength(40, ErrorMessage = "Current Highschool should be less than or equal to fourty characters.")]
         [Display(Name = "Current Highschool")]
         public string HighschoolName { get; set; }
 
+        [Required]
         [RegularExpression("^[a-zA-Z .&'-]*$", ErrorMessage = "Only Alphabetical characters allowed.")]
         [StringLength(40, ErrorMessage = "Current Highschool Coach should be less than or equal to fourty characters.")]
         [Display(Name = "Current Highschool Coach")]
         public string HighschoolCoach { get; set; }
 
+        [Required]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
         [Display(Name = "HS Graduation Date"), DataType(DataType.Date)]
         public DateTime HSGraduationDate { get; set; }
