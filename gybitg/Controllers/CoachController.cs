729000612aabfcd189539bb982cc789636f6f889
Adam Kohler <adamkohler@frontier.com>
2019-08-21 17:07:15 -0700

    commentd out some code still in development, right now the follow button works only to add an athlete to the CoachAthlete of the current coach.

--- gybitg/Controllers/CoachController.cs
+++ gybitg/Controllers/CoachController.cs
@@ -56,7 +56,70 @@ namespace gybitg.Controllers
             return View(await _context.AthleteProfiles.ToListAsync());
         }
 
-       
+        //This allows the coachId and athleteId to be linked in the database, the current coach is now "following the selected athlete"
+        [HttpGet]
+        public async Task<IActionResult> Follow(string id)
+        {
+            CoachAthlete following = new CoachAthlete();
+            following.AthleteId = id;
+            following.Athlete = _context.AthleteProfiles.SingleOrDefault(a => a.UserId == id);
+
+            following.CoachId = _userManager.GetUserId(User);
+            following.Coach = _context.CoachProfiles.SingleOrDefault(c => c.UserId == following.CoachId);
+
+            //need to add a check and make sure the coach is not already following the selected athlete
+            try
+            {
+                _context.CoachAthletes.Add(following);
+                _context.SaveChanges();
+            }
+            catch { throw new ApplicationException($"Already following that athlete."); }
+
+            //We can have this route to wherever, for right now it would go the a list of 
+              //athletes the Coach is following, but that is under development
+            return RedirectToAction("AthleteList", id);
+        }
+
+        //There have been a couple different attemps and populating a view with a list of athletes that a coach is 
+            // "following".  Below are those attempts, currently they do not work.
+        [HttpGet]
+        public async Task<IActionResult> AthleteList(string coachId)
+        {
+            // GET: Coach
+
+            var uID = _userManager.GetUserId(User);
+
+            IEnumerable<CoachAthlete> profiles = _context.CoachAthletes.Include(a => a.Athlete).Where(c => c.CoachId == uID);
+            List<AthleteProfile> followingAthletes = new List<AthleteProfile>();
+            foreach (var p in profiles)
+            {
+                followingAthletes.Add(p.Athlete);
+            }
+
+            return View(followingAthletes);
+            /*
+            //var AthleteUsers = _userManager.GetUsersInRoleAsync("Athlete");
+
+            IList<ApplicationUser> athleteUsers = await _userManager.GetUsersInRoleAsync("Athlete");
+            List<AthleteUserViewModel> athletes = new List<AthleteUserViewModel>();
+            foreach(var athlete in athleteUsers)
+            {
+                var followed = _context.CoachAthletes.Single(ath => ath.CoachId == coachId);
+                if (athlete.Id == followed.AthleteId)
+                {
+                    AthleteUserViewModel au = new AthleteUserViewModel();
+                    au.UserId = followed.AthleteId;
+                    au.Position = athlete.Position;
+                    au.FirstName = athlete.FirstName;
+                    au.LastName = athlete.LastName;
+
+                    athletes.Add(au);
+                }   
+            }
+            return View(athletes);
+            */
+        }
+
         /*[HttpGet]
         public IActionResult AthleteList(string id)
         {
@@ -86,8 +149,8 @@ namespace gybitg.Controllers
                 au.Weight = _userProfile.Weight;
                 au.HighschoolName = _userProfile.HighschoolName;
                 au.HSGraduationDate = _userProfile.HSGraduationDate;
-            
-               
+
+
 
             //    athletes.Add(au);
 
@@ -95,120 +158,6 @@ namespace gybitg.Controllers
             //}
 
             return View(au);
-
-        }*/
-
-        [HttpGet]
-        public async Task<IActionResult> FollowList(string coachId)
-        {
-            //var AthleteUsers = _userManager.GetUsersInRoleAsync("Athlete");
-
-            IList<ApplicationUser> athleteUsers = await _userManager.GetUsersInRoleAsync("Athlete");
-            List<AthleteUserViewModel> athletes = new List<AthleteUserViewModel>();
-            foreach(var athlete in athleteUsers)
-            {
-                var followed = _context.CoachAthletes.Single(ath => ath.CoachId == coachId);
-                if (athlete.Id == followed.AthleteId)
-                {
-                    AthleteUserViewModel au = new AthleteUserViewModel();
-                    au.UserId = followed.AthleteId;
-                    au.Position = athlete.Position;
-                    au.FirstName = athlete.FirstName;
-                    au.LastName = athlete.LastName;
-
-                    athletes.Add(au);
-                }   
-            }
-            return View(athletes);
-        }
-
-        //This allows the coachId and athleteId to be linked in the database, the current coach is now "following the selected athlete"
-        [HttpGet]
-        public async Task<IActionResult> Follow(string id)
-        {
-            //var athletefollowed = _context.CoachAthletes.Single(a => a.AthleteId == id);
-
-            CoachAthlete following = new CoachAthlete();
-            following.AthleteId = id;
-            following.Athlete = _context.AthleteProfiles.SingleOrDefault(a => a.UserId == id);
-
-            following.CoachId = _userManager.GetUserId(User);
-            following.Coach = _context.CoachProfiles.SingleOrDefault(c => c.UserId == following.CoachId);
-
-            //need to add a check and make sure the coach is not already following the selected athlete
-
-            try
-            {
-                _context.CoachAthletes.Add(following);
-                _context.SaveChanges();
-            }
-            catch { }
-
-            IList<ApplicationUser> athleteUsers = await _userManager.GetUsersInRoleAsync("Athlete");
-            List<AthleteUserViewModel> athletes = new List<AthleteUserViewModel>();
-            foreach (var athlete in athleteUsers)
-            {
-                //Athlete ID will equal CoachAthlete.AthleteId if the player is being followed by the logged in coach
-                //athlete.Id = 
-
-                var isFollowed = _context.CoachAthletes.SingleOrDefault(ath => athlete.Id == id);
-                //if (athlete.Id == _context.CoachAthletes.SingleOrDefault())
-
-                var followed = _context.CoachAthletes.Single(ath => ath.CoachId == id);
-                if (athlete.Id == followed.AthleteId)
-                {
-                    AthleteUserViewModel au = new AthleteUserViewModel();
-                    au.UserId = followed.AthleteId;
-                    au.Position = athlete.Position;
-                    au.FirstName = athlete.FirstName;
-                    au.LastName = athlete.LastName;
-
-                    athletes.Add(au);
-                }
-            }
-
-            return RedirectToAction("AthleteList", athletes);
-            //return View();
-        }
-
-        [HttpGet]
-        public IActionResult AthleteList(AthleteUserViewModel athletes)
-        {
-            return View(athletes);
-        }
-
-
-        public IActionResult AthletList2(string id)
-        {
-            var AthletesFollowed = _context.CoachAthletes.Where(coach => coach.CoachId == id);
-            return View();
-        }
-
-        /*[HttpGet]
-        public IActionResult Follow(string id)
-        {
-            string roleName = "Athlete";
-
-            var usersOfRole = await _userManager.GetUsersInRoleAsync(roleName);
-
-            List<AthleteUserViewModel> athletes = new List<AthleteUserViewModel>();
-
-            foreach (var u in usersOfRole)
-            {
-                AthleteUserViewModel au = new AthleteUserViewModel();
-                au.UserId = u.UserName;
-                au.FirstName = u.FirstName;
-                au.LastName = u.LastName;
-                au.Position = u.Position;
-                au.AvatarImageUrl = u.AvatarImageUrl;
-                au.ProfileVideoUrl = u.ProfileVideoUrl;
-
-                athletes.Add(au);
-
-
-            }
-
-            return View();
 
         }*/
     }
